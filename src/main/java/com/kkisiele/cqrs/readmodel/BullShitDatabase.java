e1e4eb3cfe559e6ac328202d7a2ac2afa00a74c8
Karol Kisielewski <kkisiele@gmail.com>
2018-10-14 21:15:47 +0200

    add final modifier

--- src/main/java/com/kkisiele/cqrs/readmodel/BullShitDatabase.java
+++ src/main/java/com/kkisiele/cqrs/readmodel/BullShitDatabase.java
@@ -3,6 +3,6 @@ package com.kkisiele.cqrs.readmodel;
 import java.util.*;
 
 public final class BullShitDatabase {
-    public List<InventoryItemListDto> list = new LinkedList<>();
-    public Map<UUID, InventoryItemDetailsDto> details = new HashMap<>();
+    public final List<InventoryItemListDto> list = new LinkedList<>();
+    public final Map<UUID, InventoryItemDetailsDto> details = new HashMap<>();
 }
