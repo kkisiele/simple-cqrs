1141001c2dc6f9fc7eabb4a8a0330f38c6b97e32
Karol Kisielewski <kkisiele@gmail.com>
2018-10-14 21:49:16 +0200

    class rename

--- src/test/java/com/kkisiele/cqrs/ApplicationTest.java
+++ src/test/java/com/kkisiele/cqrs/ApplicationTest.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-public class CqrsTest {
+public class ApplicationTest {
     private final Application app = new Application();
 
     @Test
