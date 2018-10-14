d90b1c0727a224d1a9a082a0d8af4b0b453c83fa
Karol Kisielewski <kkisiele@gmail.com>
2018-10-14 22:06:20 +0200

    cleanup

--- src/main/java/com/kkisiele/cqrs/infrastructure/FakeBus.java
+++ src/main/java/com/kkisiele/cqrs/infrastructure/FakeBus.java
@@ -5,27 +5,28 @@ import com.kkisiele.cqrs.*;
 import java.util.*;
 
 public final class FakeBus implements EventPublisher, CommandSender {
-    private final Map<Class<? extends Message>, List<MessageHandler>> handlers = new HashMap<>();
+    private final Map<Class<? extends Message>, List<MessageHandler>> routes = new HashMap<>();
 
     public <T extends Message> void registerHandler(Class<T> cmd, MessageHandler<T> handler) {
-        handlers.computeIfAbsent(cmd, aClass -> new LinkedList<>()).add(handler);
+        var handlers = routes.computeIfAbsent(cmd, clazz -> new LinkedList<>());
+        handlers.add(handler);
     }
 
     @Override
     public void publish(Event event) {
-        handlers.get(event.getClass()).forEach(messageHandler -> messageHandler.handle(event));
+        routes.get(event.getClass()).forEach(messageHandler -> messageHandler.handle(event));
     }
 
     @Override
     public void send(Command command) {
-        List<MessageHandler> h = handlers.get(command.getClass());
-        if(h == null) {
+        var handlers = routes.get(command.getClass());
+        if(handlers == null) {
             throw new RuntimeException("No handler registered");
         }
-        if(h.size() != 1) {
+        if(handlers.size() != 1) {
             throw new RuntimeException("Cannot send to more than one handler");
         }
-        h.get(0).handle(command);
+        handlers.get(0).handle(command);
     }
 }
 
